7227dcb853867257cb2dcbe7975390f9e2bff727
Joaco <montesano1994@gmail.com>
2023-05-05 23:51:00 -0300

    fix login

--- src/components/LoginComponent/LoginComponent.tsx
+++ src/components/LoginComponent/LoginComponent.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import style from "./LoginComponent.module.scss";
 import { AnimatePresence, motion } from "framer-motion";
-import StepAnimation from "./StepsAnimation";
+// import StepAnimation from "./StepsAnimation";
 
 export default function LoginComponent() {
   const [step, setStep] = useState(1);
@@ -47,10 +47,10 @@ export default function LoginComponent() {
   return (
     <div className={style.loginContainer}>
       <div className={style.stepsContainer}>
-        <StepAnimation step={1} currentStep={step} />
-        <StepAnimation step={2} currentStep={step} />
-        <StepAnimation step={3} currentStep={step} />
-        <StepAnimation step={4} currentStep={step} />
+        <Step step={1} currentStep={step} />
+        <Step step={2} currentStep={step} />
+        <Step step={3} currentStep={step} />
+        <Step step={4} currentStep={step} />
       </div>
       <AnimatePresence mode="wait">
         {step === 1 && (
@@ -135,3 +135,78 @@ export default function LoginComponent() {
 }
 
 
+
+
+
+function Step({
+    step,
+    currentStep,
+  }: {
+    step: number;
+    currentStep: number;
+  }): JSX.Element {
+    const status =
+      currentStep === step
+        ? "active"
+        : currentStep < step
+        ? "inactive"
+        : "complete";
+  
+    return (
+      <motion.div
+        className={style.stepCircle}
+        initial={false}
+        animate={status}
+        variants={{
+          inactive: {
+            backgroundColor: "white",
+            borderColor: "#E2E8F0",
+            color: "#E2E8F0",
+ 
+          },
+          active: {
+            backgroundColor: "#fff",
+            borderColor: "#3B82F6",
+            color: "#3B82F6",
+
+          },
+          complete: {
+            backgroundColor: "#3B82F6",
+            borderColor: "#3B82F6",
+            color: "#3B82F6",
+
+          },
+        }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="flex items-center justify-center">
+          {status === "complete" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={3}
+              stroke="white"
+              className="h-6 w-6"
+            >
+              <motion.path
+                initial={{ pathLength: 0 }}
+                animate={{ pathLength: 1 }}
+                transition={{
+                  delay: 0.1,
+                  type: "tween",
+                  ease: "easeIn",
+                  duration: 0.4,
+                }}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12.75l6 6 9-13.5"
+              />
+            </svg>
+          ) : (
+            <span>{step}</span>
+          )}
+        </div>
+      </motion.div>
+    );
+  }
--- src/components/LoginComponent/StepsAnimation.tsx
+++ src/components/LoginComponent/StepsAnimation.tsx
@@ -1,71 +1,71 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
-export default function StepAnimation({
-    step,
-    currentStep,
-  }: {
-    step: number;
-    currentStep: number;
-  }): JSX.Element {
-    const status =
-      currentStep === step
-        ? "active"
-        : currentStep < step
-        ? "inactive"
-        : "complete";
+// export default function Step({
+//     step,
+//     currentStep,
+//   }: {
+//     step: number;
+//     currentStep: number;
+//   }): JSX.Element {
+//     const status =
+//       currentStep === step
+//         ? "active"
+//         : currentStep < step
+//         ? "inactive"
+//         : "complete";
   
-    return (
-      <motion.div
-        className={`flex h-10 w-10 items-center justify-center rounded-full border-2 text-sm`}
-        initial={false}
-        animate={status}
-        variants={{
-          inactive: {
-            backgroundColor: "white",
-            borderColor: "#E2E8F0",
-            color: "#E2E8F0",
-          },
-          active: {
-            backgroundColor: "white",
-            borderColor: "#3B82F6",
-            color: "#3B82F6",
-          },
-          complete: {
-            backgroundColor: "#3B82F6",
-            borderColor: "#3B82F6",
-            color: "#3B82F6",
-          },
-        }}
-        transition={{ duration: 0.5 }}
-      >
-        <div className="flex items-center justify-center">
-          {status === "complete" ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={3}
-              stroke="white"
-              className="h-6 w-6"
-            >
-              <motion.path
-                initial={{ pathLength: 0 }}
-                animate={{ pathLength: 1 }}
-                transition={{
-                  delay: 0.1,
-                  type: "tween",
-                  ease: "easeIn",
-                  duration: 0.4,
-                }}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4.5 12.75l6 6 9-13.5"
-              />
-            </svg>
-          ) : (
-            <span>{step}</span>
-          )}
-        </div>
-      </motion.div>
-    );
-  }
+//     return (
+//       <motion.div
+//         className={`flex h-10 w-10 items-center justify-center rounded-full border-2 text-sm`}
+//         initial={false}
+//         animate={status}
+//         variants={{
+//           inactive: {
+//             backgroundColor: "white",
+//             borderColor: "#E2E8F0",
+//             color: "#E2E8F0",
+//           },
+//           active: {
+//             backgroundColor: "white",
+//             borderColor: "#3B82F6",
+//             color: "#3B82F6",
+//           },
+//           complete: {
+//             backgroundColor: "#3B82F6",
+//             borderColor: "#3B82F6",
+//             color: "#3B82F6",
+//           },
+//         }}
+//         transition={{ duration: 0.5 }}
+//       >
+//         <div className="flex items-center justify-center">
+//           {status === "complete" ? (
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               fill="none"
+//               viewBox="0 0 24 24"
+//               strokeWidth={3}
+//               stroke="white"
+//               className="h-6 w-6"
+//             >
+//               <motion.path
+//                 initial={{ pathLength: 0 }}
+//                 animate={{ pathLength: 1 }}
+//                 transition={{
+//                   delay: 0.1,
+//                   type: "tween",
+//                   ease: "easeIn",
+//                   duration: 0.4,
+//                 }}
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 d="M4.5 12.75l6 6 9-13.5"
+//               />
+//             </svg>
+//           ) : (
+//             <span>{step}</span>
+//           )}
+//         </div>
+//       </motion.div>
+//     );
+//   }
