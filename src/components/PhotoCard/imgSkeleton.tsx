dbccd74394c8157e399a4fecc0f40b988643b2a8
Joaco <montesano1994@gmail.com>
2023-05-06 15:14:00 -0300

    removed skeleton

--- src/components/PhotoCard/imgSkeleton.tsx
+++ src/components/PhotoCard/imgSkeleton.tsx
@@ -1,18 +1,18 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+// import React from "react"
+// import ContentLoader from "react-content-loader"
 
-const ImgSkeleton = (props) => (
-  <ContentLoader 
-    speed={2}
-    width={476}
-    height={340}
-    viewBox="0 0 476 340"
-    backgroundColor="#cfe8f7"
-    foregroundColor="#ecebeb"
-    {...props}
-  >
-    <rect x="55" y="42" rx="16" ry="16" width="274" height="216" />
+// const ImgSkeleton = (props) => (
+//   <ContentLoader 
+//     speed={2}
+//     width={476}
+//     height={340}
+//     viewBox="0 0 476 340"
+//     backgroundColor="#cfe8f7"
+//     foregroundColor="#ecebeb"
+//     {...props}
+//   >
+//     <rect x="55" y="42" rx="16" ry="16" width="274" height="216" />
 
-  </ContentLoader>
-)
-export default ImgSkeleton
+//   </ContentLoader>
+// )
+// export default ImgSkeleton
